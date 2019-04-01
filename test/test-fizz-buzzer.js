6ed8129163e8fc64dd4c92cb10bdcb3fb97faab6
Dave Patel <davepatel95@gmail.com>
2019-04-01 15:45:17 -0700

    updated fizz-buzzer test

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -7,29 +7,37 @@ const fizzBuzzer = require('../fizzBuzzer');
 describe('fizzBuzzer', function() {
 
     // testing normal case
-    it('should see if divisible by 3, 5, or 15', function() {
-        const normalCases = [
-            {a:9, expected:'fizz'},
-            {a:20, expected:'buzz'},
-            {a:-75, expected:'fizz-buzz'},
-        ];
-
-        normalCases.forEach(function(input) {
-            const answer = fizzBuzzer(input.a);
-            expect(answer).to.equal(input.expected);
+    it(`should return 'fizz-buzz' if divisible by 15`, function() {
+        [30,-75,1515].forEach(function(input) {
+            expect(fizzBuzzer(input)).to.equal('fizz-buzz');
         });
     });
 
+    it(`should return 'fizz' if divisible by 3`, function() {
+        [9,-27,3006].forEach(function(input) {
+            expect(fizzBuzzer(input)).to.equal('fizz');
+        });
+    });
+
+    it(`should return 'buzz' if divisible by 5`, function() {
+        [25,-70,2530].forEach(function(input) {
+            expect(fizzBuzzer(input)).to.equal('buzz');
+        });
+    });
+
+    it(`should return number if not divisible by 3 or 5`, function() {
+        [7,-127,851353].forEach(function(input) {
+            expect(fizzBuzzer(input)).to.equal(input);
+        });
+    });
+
+
     it('should raise error if arguments not numbers', function() {
         // test for bad inputs, should return error
-        const badInputs = [
-            ['a', 1],
-            ['2', 2]
-        ];
-
+        const badInputs = [true, false, function() {}, [1, 4, 5], 'dog'];
         badInputs.forEach(function(input) {
             expect(function() {
-                fizzBuzzer(input[0]);
+                fizzBuzzer(input);
             }).to.throw(Error);
         });
     });
